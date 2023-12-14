68537b971eaaa357f8b415ef13d252f26119120e
anyamaai <benjamin.babat@gmail.com>
2023-12-15 00:41:41 +0100

    file added: 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program prints string using puts()
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char string[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(string);
+	return (0);
+}
