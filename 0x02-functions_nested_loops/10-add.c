71ee27a41130718ad4a332cfe064ff129b88a519
anyamaai <benjamin.babat@gmail.com>
2023-12-21 00:11:16 +0100

    file added: 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - function adds two numbers
+ * @iNum1: first number variable
+ * @iNum2: second number variable
+ *
+ * Return: iResult
+ */
+int add(int iNum1, int iNum2)
+{
+	int iResult;
+
+	iResult = iNum1 + iNum2;
+	return (iResult);
+}
