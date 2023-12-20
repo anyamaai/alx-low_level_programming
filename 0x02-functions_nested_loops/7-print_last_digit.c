909228c67fda2e1b16bb4f71fb3c126dbe10a7c1
anyamaai <benjamin.babat@gmail.com>
2023-12-20 01:20:57 +0100

    file added: 7-print_last_digit.c main.h

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function prints last digit of a number
+ * @r: variable with number
+ *
+ * Return: r
+ */
+int print_last_digit(int r)
+{
+	int iRem;
+
+	iRem = r / 10;
+	return (iRem);
+}
