4689b2842b201ff373a2eba5f693424b70d15e54
anyamaai <benjamin.babat@gmail.com>
2023-12-21 00:49:39 +0100

    file modified: 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,6 +13,10 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
+			if (n == 98)
+			{
+				printf("%d\n", n);
+			}
 			printf("%d, ", n);
 			n++;
 		}
@@ -21,9 +25,12 @@ void print_to_98(int n)
 	{
 		while (n >= 98)
 		{
+			if (n == 98)
+			{
+				printf("%d\n", n);
+			}
 			printf("%d, ", n);
 			n--;
 		}
 	}
-	printf("\n");
 }
