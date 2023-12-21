5ce1d322b945392636db3402e299be62f1f1567f
anyamaai <benjamin.babat@gmail.com>
2023-12-21 23:16:59 +0100

    files added: main.h 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - function checks for upper character
+ * @c: variable for character checks
+ *
+ * Return: 1 or 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
