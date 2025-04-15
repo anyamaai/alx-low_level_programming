ed0323ac7f039f925dc89e6379de1ee15fb9d553
anyamaai <benjamin.babat@gmail.com>
2025-04-15 04:16:59 +0100

    fizzbuzz program file added

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - program to print numbers 1 through 100.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int iNumber;
+
+	iNumber = 1;
+	while (iNumber <= 100)
+	{
+		if (iNumber % 3 == 0 && iNumber % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (iNumber % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (iNumber % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", iNumber);
+		}
+		iNumber++;
+	}
+	printf("\n");
+	return (0);
+}
