df57d696f5266a5dec0d1723cd5fc6cf9f631f3a
anyamaai <benjamin.babat@gmail.com>
2025-04-15 03:49:19 +0100

    8-print_square file added

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_square - program prints a square
+ *
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+	int iLine1, iLine2;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		iLine1 = 1;
+		while (iLine1 <= size)
+		{
+			iLine2 = 1;
+			while (iLine2 <= size)
+			{
+				_putchar('#');
+				iLine2++;
+			}
+			_putchar('\n');
+			iLine1++;
+		}
+	}
+}
